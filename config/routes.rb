f31429a54a6815283357b50142d321fa0d7c56be
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-04 23:21:07 +0100

    update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-
-  get 'posts/edit'
-
-  get 'posts/index'
-
-  get 'posts/show'
+  root 'posts#index'
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
