430d65932ae2d4ce06162283350839dbddce2922
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-04 22:47:51 +0100

    generate PostsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
