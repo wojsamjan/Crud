fe29e44c94790d052a1f83d488fbd9682b483abb
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-06 23:15:13 +0100

    define custom helpers to use in post details view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def format_date(date)
+    date.strftime("%d.%m.%Y")
+  end
+
+  def post_footer(post)
+    "<cite>#{post.author}, #{format_date(post.created_at)}</cite>".html_safe
+  end
 end
